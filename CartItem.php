048837c6c37e69384a31e1a14b9a8c6050e086be
Amanda <aamandaallinger@outlook.com>
2022-12-05 11:47:51 +0100

    done

--- CartItem.php
+++ CartItem.php
@@ -7,13 +7,35 @@ class CartItem
     private int $quantity;
 
     // TODO Skriv en konstruktor som sätter alla properties
+    public function __construct($product, $quantity)
+    {
+        $this->product = $product;
+        $this->quantity = $quantity
+    }
 
     // TODO Skriv getters för alla properties
+    public function getQuantity() 
+    {
+        return $this->quantity;
+    }
+
+    public function getProduct() 
+    {
+        return $this->product;
+    }
 
 
     //VG: Skall utöka antalet på ett cartItem med 1. 
     //VG: Det skall inte vara möjligt att utöka så att antalet överstiger produktens $inStock.
     public function increaseQuantity()
-    {
-    }
+    //{
+       // $this -> quantity +=1;
+   
+   // } else {
+        //echo "<p>Kan inte lägga till. Slut i lager</p>";
+
+   
+    
+    
+    //}
 }
