4716d820613dabcdc6046cdc652b65602e0b8b83
Amanda <aamandaallinger@outlook.com>
2022-12-05 11:49:29 +0100

    --

--- Cart.php
+++ Cart.php
@@ -15,25 +15,44 @@ class Cart
      VG: Om produkten redan finns i kundvagnen
      skall istället quantity på cartitem ökas.
      */
-    public function addProduct($product)
+    public function addProduct($product, $quantity)
     {
+        if (isset($this->items[$product->getId()])) {
+            $this->items[$product->getId()]->increaseQuantity();
+            return $this->items[$product->getId()];
+        } else {
+            $cartItem = new CartItem($product, $quantity);
+            $this->items[$product->getId()] = $cartItem;
+            return $cartItem;
+        }
     }
 
 
     //Skall ta bort en produkt ur kundvagnen (använd unset())
     public function removeProduct($product)
     {
+        unset ($this->items[$product-getId()]);
     }
 
     //Skall returnera totala antalet produkter i kundvagnen
     //OBS: Ej antalet unika produkter
     public function getTotalQuantity()
     {
+        $totalQuantity = 0;
+        foreach ($this->items as $item) {
+            $totalQuantity += $item->getQuantity();
+        }
+        return $totalQuantity;
     }
 
     //Skall räkna ihop totalsumman för alla produkter i kundvagnen
     //VG: Tänk på att ett cartitem kan ha olika quantity
     public function getTotalSum()
     {
+        $totalSum = 0;
+        foreach ($this->items as $item) {
+            $totalSum += $item-getQuantity() * $item->getProduct()->getPrice();
+        }
+        return $totalSum;
     }
 }
